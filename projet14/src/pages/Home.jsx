f2e584567476bb9f7021e2b9a6f762e3af9e3ee9
frumilly <ouerfelli.fzahra2@gmail.com>
2024-02-27 17:42:43 +0100

    correction soutenance

--- projet14/src/pages/Home.jsx
+++ projet14/src/pages/Home.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { addEmployee } from '../redux/employeeSlice';
-
-import Modal  from 'react-customise-modal-bis';
-//import Modal from '../components/ModalBis';
+import statesData from './states.json';
+import departmentsData from './departments.json';
+import Modal from 'react-customise-modal-bis';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import Select from 'react-select';
@@ -20,83 +20,17 @@ const Home = ({ addEmployee }) => {
     startDate: '',
     street: '',
     city: '',
-    stateOptions: [
-      { value: 'AL', label: 'Alabama' },
-      { value: 'AK', label: 'Alaska' },
-      { value: 'AS', label: 'American Samoa' },
-      { value: 'AZ', label: 'Arizona' },
-      { value: 'AR', label: 'Arkansas' },
-      { value: 'CA', label: 'California' },
-      { value: 'CO', label: 'Colorado' },
-      { value: 'CT', label: 'Connecticut' },
-      { value: 'DE', label: 'Delaware' },
-      { value: 'DC', label: 'District Of Columbia' },
-      { value: 'FM', label: 'Federated States Of Micronesia' },
-      { value: 'FL', label: 'Florida' },
-      { value: 'GA', label: 'Georgia' },
-      { value: 'GU', label: 'Guam' },
-      { value: 'HI', label: 'Hawaii' },
-      { value: 'ID', label: 'Idaho' },
-      { value: 'IL', label: 'Illinois' },
-      { value: 'IN', label: 'Indiana' },
-      { value: 'IA', label: 'Iowa' },
-      { value: 'KS', label: 'Kansas' },
-      { value: 'KY', label: 'Kentucky' },
-      { value: 'LA', label: 'Louisiana' },
-      { value: 'ME', label: 'Maine' },
-      { value: 'MH', label: 'Marshall Islands' },
-      { value: 'MD', label: 'Maryland' },
-      { value: 'MA', label: 'Massachusetts' },
-      { value: 'MI', label: 'Michigan' },
-      { value: 'MN', label: 'Minnesota' },
-      { value: 'MS', label: 'Mississippi' },
-      { value: 'MO', label: 'Missouri' },
-      { value: 'MT', label: 'Montana' },
-      { value: 'NE', label: 'Nebraska' },
-      { value: 'NV', label: 'Nevada' },
-      { value: 'NH', label: 'New Hampshire' },
-      { value: 'NJ', label: 'New Jersey' },
-      { value: 'NM', label: 'New Mexico' },
-      { value: 'NY', label: 'New York' },
-      { value: 'NC', label: 'North Carolina' },
-      { value: 'ND', label: 'North Dakota' },
-      { value: 'MP', label: 'Northern Mariana Islands' },
-      { value: 'OH', label: 'Ohio' },
-      { value: 'OK', label: 'Oklahoma' },
-      { value: 'OR', label: 'Oregon' },
-      { value: 'PW', label: 'Palau' },
-      { value: 'PA', label: 'Pennsylvania' },
-      { value: 'PR', label: 'Puerto Rico' },
-      { value: 'RI', label: 'Rhode Island' },
-      { value: 'SC', label: 'South Carolina' },
-      { value: 'SD', label: 'South Dakota' },
-      { value: 'TN', label: 'Tennessee' },
-      { value: 'TX', label: 'Texas' },
-      { value: 'UT', label: 'Utah' },
-      { value: 'VT', label: 'Vermont' },
-      { value: 'VI', label: 'Virgin Islands' },
-      { value: 'VA', label: 'Virginia' },
-      { value: 'WA', label: 'Washington' },
-      { value: 'WV', label: 'West Virginia' },
-      { value: 'WI', label: 'Wisconsin' },
-      { value: 'WY', label: 'Wyoming' }
-    ],
+    stateOptions: statesData,
 
     selectedState: null,
     zipCode: '',
-    departmentOptions: [
-      { value: 'Sales', label: 'Sales' },
-      { value: 'Marketing', label: 'Marketing' },
-      { value: 'Engineering', label: 'Engineering' },
-      { value: 'Human Resources', label: 'Human Resources' },
-      { value: 'Legal', label: 'Legal' },
-    ],
+    departmentOptions: departmentsData,
     selectedDepartment: null,
     errors: {},
     isModalOpen: false,
   });
   const navigate = useNavigate();
-  //const navigate = useNavigate();
+
 
   const saveEmployee = () => {
     if (validateForm()) {
@@ -200,12 +134,7 @@ const Home = ({ addEmployee }) => {
     setState({ ...state, isModalOpen: false });
 
   };
-  const handleOkButtonClick = () => {
-    // Fermer la modal
-    // closeModal();
-    console.log("bien ");
-    // Rediriger vers la page EmployeList
-    // navigate('/EmployeeListPage');
+  const handleOkButtonClick = () => { console.log("ferme modal");
   };
   const handleOk = () => {
     // Logique à exécuter lors de l'appui sur le bouton OK de la modale
@@ -343,22 +272,15 @@ const Home = ({ addEmployee }) => {
         <button onClick={saveEmployee}>Save</button>
 
 
-        {/*   <Modal 
-        isOpen={state.isModalOpen} 
-        onClose={closeModal} 
-        message="Employee Created!" 
-        buttons={customModalButtons} 
-        onOk={handleOk} 
-        color="black" 
-        background="white" 
-      />   */}
         <Modal
-          isOpen={state.isModalOpen} 
-          onClose={closeModal} 
-          onOk={handleOk} 
-          />
-
-
+          isOpen={state.isModalOpen}
+          onClose={closeModal}
+          message="Employee Created"
+          buttons={customModalButtons}
+          onOk={handleOk}
+          textColor="blue"
+          modalBackground="white"
+        />
       </div>
     </div>
   );
